054e421743716cc74bdb60bb30433e1c88afc715
marfanr <mohammadarfan45@gmail.com>
2022-11-27 12:46:39 +0700

    feat: update

--- src/arpis_main.cpp
+++ src/arpis_main.cpp
@@ -21,25 +21,35 @@ struct arpis {
 
 class ServerNode : public rclcpp::Node {
 public:
-  ServerNode(int i) : Node("server_node") {    
+  ServerNode(int i, const char * addr, int port) : Node("server_node") {    
     // this->tcp = new arpis_network::tcp("192.168.100.3", 3333);    
-    this->tcp = new arpis_network::tcp("127.0.0.1", 3333);    
+    this->tcp = new arpis_network::tcp(addr, port);    
     tcp->set_max_con(3);
     tcp->serve();        
-    control_ = std::make_shared<tachimawari::control::DynamixelSDK>("/dev/ttyUSB0"); // sdk only    
-    joint_manager_ = std::make_shared<tachimawari::joint::JointManager>(control_);
     if (i == 1) {
+      control_ = std::make_shared<tachimawari::control::DynamixelSDK>("/dev/ttyUSB0"); // sdk only    
+      joint_manager_ = std::make_shared<tachimawari::joint::JointManager>(control_);
       if (!control_->connect()) {
         control_->set_port("/dev/ttyUSB1");
         if (!control_->connect()) {
           RCLCPP_ERROR(rclcpp::get_logger("arpis_server"), "failed connect sdk");
         }
-      }
-      timer_ = this->create_wall_timer(1s, std::bind(&ServerNode::read_join, this));        
+      }    
+      timer_ = this->create_wall_timer(8ms, std::bind(&ServerNode::read_join, this));        
     }  else if (i == 2) {
       this->generate_dummy();
       timer_ = this->create_wall_timer(800ms, std::bind(&ServerNode::read_dummy, this));        
-    }
+     } else if (i == 3) {
+        control_ = std::make_shared<tachimawari::control::CM740>("/dev/ttyUSB0"); // sdk only    
+        joint_manager_ = std::make_shared<tachimawari::joint::JointManager>(control_);
+      if (!control_->connect()) {
+        control_->set_port("/dev/ttyUSB1");
+        if (!control_->connect()) {
+          RCLCPP_ERROR(rclcpp::get_logger("arpis_server"), "failed connect cm740");
+        }
+      }
+      timer_ = this->create_wall_timer(8ms, std::bind(&ServerNode::read_join, this));        
+     }
   }
   
   void close() {
@@ -55,13 +65,10 @@ private:
     for (auto joint: joints) {
       RCLCPP_INFO(rclcpp::get_logger("arpis_server"), "s %i : %i", joint.get_id(), joint.get_position_value());          
       item.i[pos].id = static_cast<int>(joint.get_id());
-      item.i[pos].position = joint.get_position_value();
-    //   i.id = static_cast<int>(joint.get_id());
-    //   i.position = joint.get_position_value();
-    //   item[pos] = i;
+      item.i[pos].position = joint.get_position_value();  
       pos+=1;
     }
-      tcp->send(reinterpret_cast<char *>(&item), sizeof(arpis));        
+    tcp->send((char *)(void *)&item, sizeof(item));            
   }  
 
   void read_dummy() {    
@@ -109,9 +116,27 @@ int main(int argc, char ** argv)
     id = 1;
   else if (argv[1] == (std::string)"dummy") 
     id = 2;
+  if (argv[1] == (std::string)"cm740")
+    id = 3;
+
+  const char * addr = "127.0.0.1";
+  int port = 3000;
+  for (int t = 0; t < argc; t++) {
+    if (argv[t] == (std::string)"--addr")
+      addr = (const char *)argv[t+1];
+    else if (argv[t] == (std::string)"--port")
+      port = (int)atoi(argv[t+1]);
+  }
+
   
-  auto node = std::make_shared<ServerNode>(id);
-  rclcpp::spin(node);  
+  auto node = std::make_shared<ServerNode>(id, addr, port);
+  rclcpp::executors::MultiThreadedExecutor exec;
+  exec.add_node(node);
+  rclcpp::Rate rcl_rate(8ms);
+  while(rclcpp::ok()) {
+    rcl_rate.sleep();
+    exec.spin_some();  
+  }
   // node->close();
   rclcpp::shutdown();
 
