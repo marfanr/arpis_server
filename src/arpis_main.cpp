d7d10cf7a104105281c64ab9ed45d9bc8ec03df6
marfanr <mohammadarfan45@gmail.com>
2022-11-30 00:00:07 +0700

    fix: fix get realtime joint data

--- src/arpis_main.cpp
+++ src/arpis_main.cpp
@@ -7,6 +7,8 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tachimawari/control/control.hpp"
 #include "tachimawari/joint/joint.hpp"
+#include "tachimawari/joint/model/joint_id.hpp"
+#include "tachimawari/joint/protocol_1/mx28_address.hpp"
 
 using namespace std::chrono_literals;
 
@@ -36,9 +38,9 @@ public:
         }
       }    
       timer_ = this->create_wall_timer(8ms, std::bind(&ServerNode::read_join, this));        
-    }  else if (i == 2) {
+    }  else if (i == 2) { // dummy
       this->generate_dummy();
-      timer_ = this->create_wall_timer(800ms, std::bind(&ServerNode::read_dummy, this));        
+      timer_ = this->create_wall_timer(8ms, std::bind(&ServerNode::read_dummy, this));        
      } else if (i == 3) {
         control_ = std::make_shared<tachimawari::control::CM740>("/dev/ttyUSB0"); // sdk only    
         joint_manager_ = std::make_shared<tachimawari::joint::JointManager>(control_);
@@ -60,6 +62,30 @@ protected:
 private:
   void read_join() {
     auto joints = joint_manager_->get_current_joints();
+    // joint_manager_->update_current_joints_from_control_manager(joints);    
+    std::vector<tachimawari::joint::Joint> new_joints(joints);
+    for (auto & joint : new_joints) {
+      float value = tachimawari::joint::Joint::CENTER_VALUE;
+
+      int current_value = control_->read_packet(
+        joint.get_id(), tachimawari::joint::protocol_1::MX28Address::PRESENT_POSITION_L, 2);
+
+      value = (current_value == -1) ? value : current_value;
+
+      joint.set_position_value(value);
+
+    }
+      for (const auto & joint : new_joints){
+        for (auto & current_joint : joints) {
+          if (current_joint.get_id() == joint.get_id()) {
+            current_joint.set_position(joint.get_position());
+            current_joint.set_pid_gain(
+              joint.get_pid_gain()[0], joint.get_pid_gain()[1], joint.get_pid_gain()[2]);
+
+            break;
+          }
+        }
+      }
     arpis item;
     int pos = 0;
     for (auto joint: joints) {
@@ -67,7 +93,7 @@ private:
       item.i[pos].id = static_cast<int>(joint.get_id());
       item.i[pos].position = joint.get_position_value();  
       pos+=1;
-    }
+    }    
     tcp->send((char *)(void *)&item, sizeof(item));            
   }  
 
@@ -75,7 +101,7 @@ private:
     // int pos = 0;
     arpis item;
     auto dummy = dummy_item_[dummy_readed];
-    for (int i = 0; i < 22; i++) {
+    for (int i = 0; i < 20; i++) {
       item.i[i].id= dummy[i].id;
       item.i[i].position = dummy[i].position;
       // pos += 1;
@@ -84,13 +110,38 @@ private:
     // i.position = dummy.position;
     tcp->send((char *)(void *)&item, sizeof(item));            
     dummy_readed += 1;
+    if (dummy_readed == 1) 
+      dummy_readed = 0;
   }  
 
   void generate_dummy() {
-    dummy_item_[0][0] = {id: 20, position: 4096};
-    dummy_item_[0][1] = {id: 19, position: 4096};
-    dummy_item_[0][2] = {id: 19, position: 4096};
-    dummy_item_[0][3] = {id: 15, position: 4096};
+    for (int i = 0; i < 1000; i ++) {
+      for (int x = 0; x < 20; x++) {
+        dummy_item_[i][x].id = x;
+        dummy_item_[i][x].position = 2048;
+      }
+    }
+    dummy_item_[0][0] = {id: 1, position: 2069};
+    dummy_item_[0][1] = {id: 1, position: 2080};
+    dummy_item_[0][2] = {id: 1, position: 2100};
+    dummy_item_[0][3] = {id: 1, position: 1970};
+    dummy_item_[0][4] = {id: 1, position: 2400};
+    dummy_item_[0][5] = {id: 1, position: 2600};
+    dummy_item_[0][6] = {id: 1, position: 2080};
+    dummy_item_[0][7] = {id: 1, position: 2090};   
+    dummy_item_[0][8] = {id: 1, position: 2090};   
+    dummy_item_[0][9] = {id: 1, position: 2090};   
+    dummy_item_[0][10] = {id: 1, position: 2090};   
+    dummy_item_[0][11] = {id: 1, position: 2090};   
+    dummy_item_[0][12] = {id: 1, position: 2100};   
+    dummy_item_[0][13] = {id: 1, position: 2100};   
+    dummy_item_[0][14] = {id: 1, position: 2100};   
+    dummy_item_[0][15] = {id: 1, position: 2100};   
+    dummy_item_[0][16] = {id: 2, position: 2500};   
+    dummy_item_[0][17] = {id: 2, position: 2300};   
+    dummy_item_[0][18] = {id: 2, position: 2500};   
+    dummy_item_[0][19] = {id: 2, position: 2700};   
+    
     
   }
 
@@ -98,7 +149,7 @@ private:
   rclcpp::TimerBase::SharedPtr timer_;  
   std::shared_ptr<tachimawari::control::ControlManager> control_;
   std::shared_ptr<tachimawari::joint::JointManager> joint_manager_;
-  arpis_transfer_item dummy_item_[100][100];
+  arpis_transfer_item dummy_item_[1000][20];
   int dummy_readed = 0;
 };
 
